8906d99701ded6d6ed7727305f907877941d3c50
Travis Sims <72163569+Sivartsmis@users.noreply.github.com>
2024-08-16 15:41:16 -0500

    created store.js
    started work on javascript to handle the cart functionality. began work on remove function to remove items form the cart

---
+++ store.js
@@ -0,0 +1,9 @@
+const removeCartItemButtons = document.getElementsByClassName("button-danger");
+
+for (let i=0; i < removeCartItemButtons.length; i++){
+    let button = removeCartItemButtons[i];
+    button.addEventListener('click', function(event) {
+        let buttonClicked = event.target;
+        buttonClicked.parentElement.parentElement.remove()
+    })
+}
